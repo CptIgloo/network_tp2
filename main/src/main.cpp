405a6d5d8c8fa7d73227fb495bb955aac5b70cbb
Fab <fab.mckay@gmail.com>
2019-10-17 16:40:17 -0400

    :construction: Main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 
 #include <server.hpp>
+#include <client.hpp>
 
-#include <uvw.hpp>
-#include <memory>
-
-int main() {
-    Server server = Server("127.0.0.1",4242);
-	uint8_t test[1] = {0x34};
-
-    while(true)
+int main(int argc, char *argv[]) 
+{
+    if(argc != 4)
     {
-        server.run();
-        server.send(test,1);
+        std::cout<<"Error : Incorrect number of arguments"<<std::endl;
+
+        #ifdef _WIN32
+        std::cout<<"Expected : tp2.exe TYPE IP PORT"<<std::endl;
+        #else
+        std::cout<<"Expected : /tp2.a TYPE IP PORT"<<std::endl;
+        #endif
+        
+        return EXIT_FAILURE;
     }
+
+    std::string type = argv[1];
+
+    if(type != "server" && type != "client")
+    {
+        std::cout<<"Error : Incorrect type argument"<<std::endl;
+        std::cout<<"Expected : server or client"<<std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string ip = argv[2];
+
+    int port = std::stoi(argv[3]);
+
+    std::cout<<"Executing "<<type<<" at ip "<<ip<<" with port "<<port<<std::endl;
+
+    return EXIT_SUCCESS;
 }
